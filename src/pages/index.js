16e1b0d0db1ee34c2a8ebd25a230c6970a2c8f8e
Mike Hodnick <mike@kindohm.com>
2019-04-10 17:23:49 -0500

    add some video and audio highlights

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -37,6 +36,18 @@ const IndexPage = () => (
         <a href="//twitter.com/kindohm">Twitter</a>
       </li>
     </ul>
+
+    <div>
+      <iframe title="Algorithmic Art Assembly" width="560" height="315" src="https://www.youtube.com/embed/RiEq_h5UQJM" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br></br>
+      <iframe title="Live Coding Conference 2016, Hamilton" width="560" height="315" src="https://www.youtube.com/embed/smQOiFt8e4Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br></br>
+      <iframe title="Mesabi Range" style={{border: 0, width: '400px', height: '406px'}} src="https://bandcamp.com/EmbeddedPlayer/album=3918382788/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless><a href="http://nadarecs.bandcamp.com/album/kindohm-mesabi-range">Kindohm - Mesabi Range by Kindohm</a></iframe>
+      <br></br>
+      <iframe title="RISC Chip" style={{border: 0, width: '400px', height: '406px'}} src="https://bandcamp.com/EmbeddedPlayer/album=2609768379/size=large/bgcol=ffffff/linkcol=0687f5/artwork=small/transparent=true/" seamless><a href="http://shop.conditional.club/album/risc-chip">RISC Chip by Kindohm</a></iframe>
+    </div>
+
+
   </Layout>
 )
 
